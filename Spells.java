a315e7e88c22904cc19c962f893be314fa57faaa
Drew Hoover <drewhoover@gmail.com>
2014-04-21 00:57:02 -0500

    This fixes some issues with Voldemort, Spells, and updates class images to be more true to HP theme.

--- Spells.java
+++ Spells.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -11,11 +10,12 @@ public class Spells extends Projectile
 {
     /** Spell loses life each act as long as nothing is in range to hit. Eventually disappears
      */
-    private int splife = 10;
+    private int splife = 100;
     /** 
      * The amount of damage the spells have.
      */
     private int damage = 1;
+    private boolean canSeePlayer = false;
     public static final Vector gravity = new Vector(90, GRAVITY);
     public Spells(Vector vector) 
     {
@@ -30,21 +30,26 @@ public class Spells extends Projectile
      */
     public void act()
     {
-        if(splife <= 0) {
+        if(splife <= 0 || getX() == 0 || getX() == 900 || getY() == 0 || getY() == 600) {
             getWorld().removeObject(this);
         } 
         else {
             move();
-            Player player = (Player) getOneIntersectingObject(Player.class);
-            // Player player = (Player)getWorld().getObjects(Player.class).get(0);
-            if (player != null) {
-                player.hit(damage);
-            }
-            else {
-                splife--;
-            }
+            destroy();
+            splife--;
         }
-    }  
+    }
+    
+    public void destroy() {
+      if (canSee(Player.class) && !canSeePlayer) {
+         Player player = (Player)getWorld().getObjects(Player.class).get(0);
+         player.hit(damage);
+         canSeePlayer = true;
+      }
+      if (!canSee(Player.class) && canSeePlayer) {
+         canSeePlayer = false;
+      }
+   }
 
     public void dragY() {
         int angle = (movement.dy > 0) ? 90 : 270;
@@ -63,72 +68,3 @@ public class Spells extends Projectile
     }
 }
 
-=======
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Spells here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Spells extends Projectile
-{
-    /** Spell loses life each act as long as nothing is in range to hit. Eventually disappears
-     */
-    private int splife = 15;
-    /** 
-     * The amount of damge the spells have.
-     */
-    private int damage = 1;
-    public static final Vector gravity = new Vector(90, GRAVITY);
-    public Spells(Vector vector) 
-    {
-        movement = vector;
-        Greenfoot.playSound("");
-        int damage = 1;
-        travel();
-    }
-
-    /**
-     * Act - do whatever the Spells wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        if(splife <= 0) {
-            getWorld().removeObject(this);
-        } 
-        else {
-            move();
-            // spells = (Spells) getOneIntersectingObject(Spells.class);
-            Seeker seeker = (Seeker)getWorld().getObjects(Seeker.class).get(0);
-            Player player = (Player)getWorld().getObjects(Player.class).get(0);
-            if(canSee (Seeker.class)) {
-                seeker.hit(damage);
-            }
-            else if (canSee( Player.class)) {
-                player.hit(damage);
-            }
-            else {
-                splife--;
-            }
-        }
-    }  
-
-    public void travel() {
-        addForce(gravity);
-        int angle = (movement.dx < 0) ? 0 : 180;
-        addForce (new Vector(angle, 0.5));
-        angle = (movement.dy > 0) ? 90 : 270;
-        addForce (new Vector(angle, 0.5));
-    }
-
-    public boolean canSee(Class clss)
-    {
-        Actor actor = getOneObjectAtOffset(0, 0, clss);
-        return actor != null;        
-    }
-}
-
->>>>>>> fa0bb712bd95593cd713296ac0e58cd81ee45cb4
--- Voldemort.java
+++ Voldemort.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
@@ -123,72 +122,3 @@ public class Voldemort extends Projectile
     }
 
 }
-=======
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class Voldemort here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class Voldemort extends Projectile
-{
-    private int reloadDelayCount;
-    private int wandReload;
-    public Voldemort() {
-        reloadDelayCount = 0;
-        wandReload = 5;
-    }
-
-    /**
-     * Act - do whatever the Voldemort wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        fire();
-        if(reloadDelayCount >= wandReload) {
-            reloadDelayCount++;
-        }
-        if (atWorldEdgeVertical() == true) {
-            movement.revertVertical(1);
-        }
-        if (atWorldEdgeHorizontal() == true) {
-            movement.revertHorizontal(1);
-        }
-    }   
-
-    /**
-     * Fire a spell;
-     */
-    private void fire() 
-    {
-        Seeker seeker = (Seeker)getWorld().getObjects(Seeker.class).get(0);
-        Player player = (Player)getWorld().getObjects(Player.class).get(0);
-        if (player.getX() == getX()) {
-            int angle = (getY() > player.getY()) ? 270 : 90;
-            Spells sp = new Spells(new Vector(10, angle));
-            getWorld().addObject(sp, getX(), getY());
-            reloadDelayCount = 0;
-        }
-        if (player.getY() == getY()) {
-            int angle = (getX() > player.getX()) ? 180 : 0;
-            Spells sp = new Spells(new Vector(10, angle));
-            getWorld().addObject(sp, getX(), getY());
-            reloadDelayCount = 0;
-        }             
-    }
-
-    public void dragY() {
-        int angle = (movement.dy > 0) ? 90 : 270;
-        addForce (new Vector(angle, 0.5));
-    }
-
-    public void dragX() {
-        int angle = (movement.dx < 0) ? 0 : 180;
-        addForce (new Vector(angle, 0.5));
-    }
-
-}
->>>>>>> fa0bb712bd95593cd713296ac0e58cd81ee45cb4
